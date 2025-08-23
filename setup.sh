8e4047edf809f947ef8f9659dc5be007d7cb13d9
vincentve42 <vincentve42@gmail.com>
2025-08-23 23:10:06 +0700

    Logic Implementation on Bash

--- setup.sh
+++ setup.sh
@@ -1,30 +1,68 @@
 #!/bin/bash
 
-echo "LAMP Installer"
-echo "1.Install Apache2"
-echo "2.Install PHP"
-echo "3.Install Mysql-server"
-echo "4.Install Phpmyadmin"
-echo "5.Install All"
-echo "6.Exit"
+mantap=1;
 
-read -sp "Enter Your Option : " option
+#if
+while [ true ]; do
+    echo "LAMP Installer";
+    echo "1.Install Apache2";
+    echo "2.Install PHP";
+    echo "3.Install Mysql-server";
+    echo "4.Install Phpmyadmin";
+    echo "5.Install All";
+    echo "6.Exit";
+
+    
+    # logic
+
+    read option;
+    if [ $option -eq 1 ]; then 
+        echo "Installing Apache 2 ....";
+    elif [ $option -eq 2 ]; then 
+        echo "Installing PHP ....";
+    elif [ $option -eq 3 ]; then 
+        echo "Instaling Mysql....";
+    elif [ $option -eq 4 ]; then 
+        echo "Instaling Phpmyadmin....";
+    elif [ $option -eq 5 ]; then 
+        echo "Instaling All....";
+    else
+        echo "Exiting App.....";
+        break
+    fi
+done
+    
+# apache2
 
 install_apache2(){
-
+    sudo apt-get update
 }
+
+# php
+
 install_php(){
+    sudo apt-get update
 
 }
+
+# mysql
+
 install_mysql()
 {
+    sudo apt-get update
 
 }
+
+# phpmyadmin
+
 install_phpmyadmin()
 {
-
+    sudo apt-get update
 }
+
+#install all
+
 install_all()
 {
-    
+    sudo apt-get update
 }
