14520ba8df5699fe3e5bbe867d6484ccd8f66542
vincentve42 <vincentve42@gmail.com>
2025-08-23 22:40:18 +0700

    First Commit

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "LAMP Installer"
+echo "1.Install Apache2"
+echo "2.Install PHP"
+echo "3.Install Mysql-server"
+echo "4.Install Phpmyadmin"
+echo "5.Install All"
+echo "6.Exit"
+
+read -sp "Enter Your Option : " option
+
+install_apache2(){
+
+}
+install_php(){
+
+}
+install_mysql()
+{
+
+}
+install_phpmyadmin()
+{
+
+}
+install_all()
+{
+    
+}
